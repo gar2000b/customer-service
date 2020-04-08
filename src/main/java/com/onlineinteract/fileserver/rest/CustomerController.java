bbb346d39438e459c641bb3b8a87991331e06f6e
Gary Black <gary.black@pcbank.ca>
2020-04-08 14:02:12 -0400

    priting headers

--- src/main/java/com/onlineinteract/fileserver/rest/CustomerController.java
+++ src/main/java/com/onlineinteract/fileserver/rest/CustomerController.java
@@ -1,13 +1,16 @@
 package com.onlineinteract.fileserver.rest;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,8 +24,15 @@ public class CustomerController {
 
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json", value = "/fetchCustomer/{customerId}")
 	@ResponseBody
-	public ResponseEntity<String> fetchCustomer(@PathVariable String customerId) {
+	public ResponseEntity<String> fetchCustomer(@PathVariable String customerId, @RequestHeader HttpHeaders incomingHeaders) {
 		System.out.println("\nFetching customer with customer ID: " + customerId);
+		
+		System.out.println("Headers: ");
+	    Iterator it = incomingHeaders.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        System.out.println(pair.getKey() + " = " + pair.getValue());
+	    }
 
 		Map<String, String> customer = new HashMap<String, String>();
 		customer.put("CustomerId", customerId);
