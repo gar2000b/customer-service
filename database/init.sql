28e92c41e3c6ca30d07b54bf52b22f8e96f21fd1
Gary Black <gar2000b@yahoo.com>
2020-04-09 14:37:11 -0400

    Updating with new database

---
+++ database/init.sql
@@ -0,0 +1,6 @@
+create table users (user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, phone_number TEXT);
+insert into users (name, phone_number) values ('Alex', '+1 416 837 2913');
+insert into users (name, phone_number) values ('Josephine', '+1 416 837 2914');
+insert into users (name, phone_number) values ('Rex', '+1 416 837 2915');
+insert into users (name, phone_number) values ('Jill', '+1 416 837 2916');
+insert into users (name, phone_number) values ('Paul', '+1 416 837 2917');
